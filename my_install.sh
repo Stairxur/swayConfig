943c2b14b2a4ce5933c3b27871cf2f24e4b2f9ec
Stairxur <stairxur@gmail.com>
2024-09-12 06:41:59 +0300

    12.09.24-1

--- my_install.sh
+++ my_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Installing programs..."
-sudo pacman -S sway swaybg waybar xorg xorg-xinit firefox vim neovim alacritty mc btop cmake smplayer cmus wofi evince numlockx udiskie pamixer grim slurp zip unzip
+sudo pacman -S sway swaybg waybar xorg xorg-xinit firefox vim alacritty mc btop cmake smplayer cmus wofi evince numlockx udiskie pamixer grim slurp zip unzip
 
 echo "Installing fonts..."
 sudo pacman -S ttf-liberation ttf-dejavu opendesktop-fonts ttf-bitstream-vera ttf-arphic-ukai ttf-arphic-uming ttf-hanazono ttf-jetbrains-mono-nerd
@@ -10,16 +10,3 @@ echo "Copying settings..."
 cp -r .config ~/
 cp -r .bashrc ~/
 cp -r .vimrc ~/
-
-echo "Install Yaourt..."
-sudo pacman -S --needed base-devel git wget yajl
-cd /tmp
-git clone https://aur.archlinux.org/package-query.git
-cd package-query/
-makepkg -si
-cd ..
-git clone https://aur.archlinux.org/yaourt.git
-cd yaourt/
-makepkg -si
-cd ..
-sudo rm -dR yaourt/ package-query/
